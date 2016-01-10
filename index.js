dc1415612c2cf83aa853f0793ae18ce6b5859493
Raquel Moss <raquelxmoss@gmail.com>
2016-01-10 22:07:02 +1300

    Add initial skeleton

---
+++ index.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+
+import App from './containers/App';
+// import writer from './reducers/writer';
+
+// let store = createStore(writer)
+
+ReactDOM.render(
+  // <Provider store={store}>
+    <App />
+  // </Provider>,
+  ,document.getElementById('app')
+)
+
