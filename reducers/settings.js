52bd906352641b552c548fb113c58be64f3499ba
Raquel Moss <raquelxmoss@gmail.com>
2016-01-15 16:52:01 +1300

    Save settings & text to 🍪

--- reducers/settings.js
+++ reducers/settings.js
@@ -1,3 +1,5 @@
+import Cookie from 'js-cookie';
+
 import { UPDATE_SETTINGS, RESET_SETTINGS, TOGGLE_VISIBILITY } from '../actions/settings_actions';
 
 const defaultSettings = {
@@ -9,15 +11,29 @@ const defaultSettings = {
   displaySettings: 'none'
 }
 
-export const settings = (state = defaultSettings, action) => {
+export const settings = (state = JSON.parse(Cookie.get('settings')) || defaultSettings, action) => {
   switch(action.type) {
-    case UPDATE_SETTINGS:
-      return Object.assign({}, state, action.settings)
-    case RESET_SETTINGS:
+    case UPDATE_SETTINGS: {
+      const newSettings = Object.assign({}, state, action.settings)
+
+      Cookie.set('settings', newSettings);
+
+      return newSettings
+    }
+    case RESET_SETTINGS: {
+      Cookie.set('settings', defaultSettings)
+
       return Object.assign({}, state, defaultSettings)
-    case TOGGLE_VISIBILITY:
-      let displaySettings = state.displaySettings === 'none' ? 'block' : 'none'
-      return Object.assign({}, state, {displaySettings: displaySettings})
+    }
+    case TOGGLE_VISIBILITY: {
+      const displaySettings = state.displaySettings === 'none' ? 'block' : 'none'
+
+      const newSettings = Object.assign({}, state, {displaySettings: displaySettings})
+
+      Cookie.set('settings', newSettings)
+
+      return newSettings
+    }
     default:
       return state
   }
